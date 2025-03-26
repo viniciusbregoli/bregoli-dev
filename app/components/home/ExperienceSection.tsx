9c92b80b7dbf703c4e826e98ffd0a42a1896f0d3
Vinicius Bregoli <vinibregoli@gmail.com>
2025-03-27 00:43:11 +0100

    Add experience section with timeline feature

--- app/components/home/ExperienceSection.tsx
+++ app/components/home/ExperienceSection.tsx
@@ -14,9 +14,20 @@ export default function ExperienceSection() {
       <div className="max-w-6xl mx-auto px-4">
         <SectionTitle>{t('experience.title')}</SectionTitle>
 
-        <div className="space-y-8">
+        <div className="relative space-y-8">
+          {/* Timeline line */}
+          <div className="absolute left-4 md:left-1/2 top-0 bottom-0 w-0.5 bg-gray-200 dark:bg-gray-700 transform -translate-x-1/2"></div>
+          
           {experiences.map((experience, index) => (
-            <ExperienceCard key={index} experience={experience} currentLanguage={language} />
+            <div key={index} className="relative">
+              {/* Timeline dot */}
+              <div className="absolute left-4 md:left-1/2 w-4 h-4 bg-blue-500 rounded-full transform -translate-x-1/2 -translate-y-1/2 z-10"></div>
+              
+              {/* Experience card */}
+              <div className={`ml-12 md:ml-0 md:w-1/2 ${index % 2 === 0 ? 'md:pr-8' : 'md:ml-auto md:pl-8'}`}>
+                <ExperienceCard experience={experience} currentLanguage={language} />
+              </div>
+            </div>
           ))}
         </div>
       </div>
